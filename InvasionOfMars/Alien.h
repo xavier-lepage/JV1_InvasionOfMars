552eac7848d4f1545855d713ae915d3e9f7cdb0f
xavier-lepage <2445275@csfoy.ca>
2026-03-15 15:01:36 -0400

    Added spawn logic to Alien

--- InvasionOfMars/Alien.h
+++ InvasionOfMars/Alien.h
@@ -6,7 +6,14 @@ class Alien : public GameObject
 public:
 	Alien();
 	void init(const int alienTextureNumber);
+	static void setPlayer(const GameObject* player);
+	Vector2f findSpawnPosition() const;
+	void spawn();
+	static Alien* getAvailableAlien();
 
 private:
+	void addToAlienStack();
 
+	static std::stack<Alien*> alienStack;
+	static const GameObject* player;
 };
